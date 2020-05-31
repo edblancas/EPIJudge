83406e3f4a55424f6f6cc55bd0f27b0c0483b991
Daniel Blancas <edblancas@gmail.com>
2020-05-30 19:56:59 -0500

    Add 6: Strings

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def is_palindromic(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    mid_len = len(s) // 2
+    if mid_len == 0:
+        return True
+
+    left = range(mid_len)
+    right = range(-1, -(mid_len+1), -1)
+
+    return all(s[i] == s[j] for i, j in zip(left, right))
+
+
+def is_palindromic_book(s: str) -> bool:
+    # the xor bitwise ~ has the effect of -(x+1)
+    return all(s[x] == s[~x] for x in range(len(s)))
 
 
 if __name__ == '__main__':
