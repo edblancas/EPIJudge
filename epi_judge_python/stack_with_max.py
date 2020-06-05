bf56076031c53623e615d3a689e898e11f90b430
Daniel Blancas <edblancas@gmail.com>
2020-06-04 19:26:01 -0500

    8 stack max

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -3,20 +3,30 @@ from test_framework.test_failure import TestFailure
 
 
 class Stack:
+    def __init__(self):
+        self.s = []
+        self.max_so_far = []
+        self.max_ = float('-inf')
+
     def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+        return len(self.s) == 0
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self.max_
 
     def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        r = self.s.pop()
+        self.max_so_far.pop()
+        if self.empty():
+            self.max_ = float('-inf')
+        else:
+            self.max_ = self.max_so_far[-1]
+        return r
 
     def push(self, x: int) -> None:
-        # TODO - you fill in here.
+        self.max_ = max(self.max_, x)
+        self.max_so_far.append(self.max_)
+        self.s.append(x)
         return
 
 
