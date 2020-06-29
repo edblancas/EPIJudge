8cef34385a2643bc3f9a65718c4e27645bbb75cf
Daniel Blancas <edblancas@gmail.com>
2020-06-28 20:48:54 -0500

    Added dP problem with tabulation

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,8 +5,28 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if final_score == 0:
+        return 1
+
+    scores = individual_play_scores
+    total = final_score
+    rows = range(len(scores))
+    cols = range(total + 1)
+    t = [[0 for _ in cols] for _ in rows]
+    for row in rows:
+        curr_scores_range = range(row + 1)
+        for col in cols:
+            curr_total = col
+            sum_ways = 0
+            for curr_score_idx in curr_scores_range:
+                delta = curr_total - scores[curr_score_idx]
+                if delta >= 0:
+                    if delta == 0:
+                        sum_ways += 1
+                    else:
+                        sum_ways += t[curr_score_idx][delta]
+            t[row][col] = sum_ways
+    return t[-1][-1]
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -1974,7 +1974,7 @@ problem_mapping = {
                 "total": 10
             },
             "Python: hanoi.py": {
-                "passed": 0,
+                "passed": 10,
                 "total": 10
             }
         },
@@ -2142,7 +2142,7 @@ problem_mapping = {
                 "total": 46
             },
             "Python: max_sum_subarray.py": {
-                "passed": 0,
+                "passed": 1001,
                 "total": 1001
             }
         },
@@ -2156,7 +2156,7 @@ problem_mapping = {
                 "total": 1003
             },
             "Python: number_of_score_combinations.py": {
-                "passed": 0,
+                "passed": 1003,
                 "total": 1003
             }
         },
